73c1af3050843e8e9592dafbc3f6f44cdb523495
Validifyed <Validifyed@users.noreply.github.com>
2017-02-10 21:33:52 +0000

    Update main.c

--- main.c
+++ main.c
@@ -3,19 +3,15 @@
 int main()
 {
 	DDRB=_BV(PB7); 
-	/* DDRE = ~_BV(PE7); */
+	/* DDRE = ~_BV(PE7); Implicitly an input by default */
 
-	PORTE |= _BV(PE7); 
+	PORTE |= _BV(PE7); /* Pullup */
 	
 	while(1)
 	{
-		if ( PINE & _BV(PE7) )
-		{   
-			PORTB &= ~_BV(PE7);
-		}	
+		if ( (PINE & _BV(PE7)) == 0 ) /* If E7 is active ie. > 0 */
+			PORTB = 128;  /* LED On */	
 		else 
-		{
-			PORTB = 128; 
-		}	
+			PORTB &= ~_BV(PE7); /* LED Off */
 	}
 }
